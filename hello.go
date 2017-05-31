799bbca904759bcd010d653cbb12f312eaab89f5
calvinwhole-github <calvinwhole-github@gmail.com>
2017-05-31 01:26:27 -0700

    added hello.go

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello, world, again.\n")
+	fmt.Printf("Hello, world, again again.\n")
+	fmt.Printf("Hello, world, again again again.\n")
+
+}
+
